526a9db2af105a1d914ab31fdffb8b432915e838
K <kwailamchan@hotmail.com>
2017-11-28 18:00:23 +0800

    [master] implemented preprocessing in image.cc. 	modified:   image.cc

--- cuda/image.cc
+++ cuda/image.cc
@@ -5,6 +5,9 @@
 #include "utils.hh"
 #include "image.hh"
 
+cv::Mat imageInputRGBA;
+cv::Mat imageOutputRGBA;
+
 void image_preprocessing (unsigned char ** h_inputImageRGBA,
                           unsigned char ** h_outputImageRGBA,
                           unsigned char ** d_inputImageRGBA,
@@ -19,4 +22,20 @@ void image_preprocessing (unsigned char ** h_inputImageRGBA,
 	checkCudaErrors(cudaFree(0));
 	
 	cv::Mat image = cv::imread(filename.c_str(), CV_LOAD_IMAGE_COLOR);
+	if (image.empty())
+	{
+		std::cerr << "Couldn't open file " << filename << std::endl;
+		exit(1);
+	}
+
+	cv::cvColor(image, imageInputRGBA, CV_BGR2BGRA);
+	imageOutputRGBA.create(image.rows, image.cols, CV_8UC4);
+	if (!imageInputRGBA.isContinuous() || !imageOutputRGBA.isContinuous())
+	{
+		std::cerr << "Images aren't continuous. Exiting." << std::endl;
+		exit(1);
+	}
+
+	* h_inputImageRGBA = (unsigned char*)imageInputRGBA.ptr<unsigned char>(0);
+	* h_outputImageRGBA = (unsigned char*)imageOutputRGBA.ptr<unsigned char>(0);
 }
